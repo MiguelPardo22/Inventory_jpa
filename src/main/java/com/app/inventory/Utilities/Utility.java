9f526cb8889fd23357f90de9859e9fbb75fae991
Miguel Pardo <115657627+MiguelPardo22@users.noreply.github.com>
2023-03-08 16:58:21 -0500

    Add files via upload

---
+++ src/main/java/com/app/inventory/Utilities/Utility.java
@@ -0,0 +1,12 @@
+package com.app.inventory.Utilities;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class Utility {
+
+	public static String getSiteUrl(HttpServletRequest request) {
+		String siteURL = request.getRequestURL().toString();
+		return siteURL.replace(request.getServletPath(), "");
+	}
+	
+}
