2ea6ce358a19777440a2a91540d374a5022b124c
Miguel Pardo <115657627+MiguelPardo22@users.noreply.github.com>
2023-03-07 19:16:35 -0500

    Add files via upload

--- src/main/java/com/app/inventory/Repository/OrderRepository.java
+++ src/main/java/com/app/inventory/Repository/OrderRepository.java
@@ -1,6 +1,9 @@
 package com.app.inventory.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.app.inventory.model.Order;
@@ -8,6 +11,16 @@ import com.app.inventory.model.Order;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer>{
 
+	@Query(value = "SELECT * FROM inventory_jpa.pedido where pedido.id_est_fk = 1;",
+			nativeQuery = true)
+	List<Order> EstadoEnProceso();
 	
+	@Query(value = "SELECT * FROM inventory_jpa.pedido where pedido.id_est_fk = 2;",
+			nativeQuery = true)
+	List<Order> EstadoPagado();
+	
+	@Query(value = "SELECT * FROM inventory_jpa.pedido where pedido.id_est_fk = 3;",
+			nativeQuery = true)
+	List<Order> EstadoSinPagar();
 	
 }
