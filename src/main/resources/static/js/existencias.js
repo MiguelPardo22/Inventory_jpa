2bf7688c8e13cbb3b63717b86ed255cfbd1ce7e5
Miguel Pardo <115657627+MiguelPardo22@users.noreply.github.com>
2023-03-13 20:13:51 -0500

    Add files via upload

--- src/main/resources/static/js/existencias.js
+++ src/main/resources/static/js/existencias.js
@@ -1,16 +1,62 @@
 $(document).ready(function() {
 	$('#myTable').DataTable({
-		ajax: {
-			url: 'http://localhost:8090/inventario'
+
+       ajax: {
+			url: 'http://localhost:8090/inventario',
+
 		},
 		columns: [
-			{ data: "id_inventario" },
-			{ data: "exis" },
+			{
+                data: "exis",
+                render: function(data) {
+                    if (data <= 100) {
+                        return '<span class="" style="color: red;">'+data+'&nbsp;&nbsp;&nbsp; <a href="/admin/PurchaseWEB" style="color: red" target="_blank">Por favor es necesario comprar aqui</a> </span>';
+                    } else {
+                        return '<span class="" style="color: green;">'+data+'</span>';
+                    }
+                }
+            },
 			{ data: "id_prod_fk.nom" },
+			
+		],
+
+    language: {
+			"lengthMenu": "Mostrar _MENU_ registros",
+			"zeroRecords": "No se encontraron resultados",
+			"info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+			"infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+			"infoFiltered": "(filtrado de un total de _MAX_ registros)",
+			"sSearch": "Buscar:",
+			"oPaginate": {
+				"sFirst": "Primero",
+				"sLast": "Último",
+				"sNext": "Siguiente",
+				"sPrevious": "Anterior"
+			},
+			"sProcessing": "Procesando...",
+		},
+
+		responsive: "true",
+		dom: 'Bfrtilp',
+		buttons: [
+			{
+				extend: 'excelHtml5',
+				text: '<i class="fas fa-file-excel"></i> ',
+				titleAttr: 'Exportar a Excel',
+				className: 'btn btn-success'
+			},
+			{
+				extend: 'pdfHtml5',
+				text: '<i class="fas fa-file-pdf"></i> ',
+				titleAttr: 'Exportar a PDF',
+				className: 'btn btn-danger'
+			},
 		]
-		
-		 });
+
+	});
 
 });
+
+
 
 
